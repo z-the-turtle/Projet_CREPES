bae0bd89b22de50d6bdd71a55d7d076b9f197a08
lilasbsn <lilas.leonie.besson@gmail.com>
2025-06-24 08:50:32 +0200

    Update modele convection nouveau.py

--- Modélisation convection/Ancienscodesconvection/modele convection nouveau.py
+++ Modélisation convection/Ancienscodesconvection/modele convection nouveau.py
@@ -3,10 +3,20 @@ import matplotlib.patches as patches
 import matplotlib.animation as animation
 import numpy as np
 
-sol_l = 36e6
-sol_h = 250e3
-air_h = 10e3
+sol_l = 36e6          #largeur du sol
+sol_h = 250e3         #hauteur du sol
+air_h = 10e3          #hauteur des blocs d'air
 
+
+# Grandeurs physiques
+m = 1.0      #masse du bloc d'air (kg)
+c = 1000.0   #capacité thermique massique (J/Kg/K)
+A = 1.0      #surface d'échange thermique (m²)
+h = 15.0     #coefficient d'échange thermique (W/(m²·K))
+dt = 1.0     #pas de temps (s)
+k = (h * A) / (m * c)     # coefficient thermique combiné (en 1/s) pour la loi de Newton
+
+# Température du sol
 nb_blocs = 18
 bloc_l = sol_l / nb_blocs
 vitesse = 10
